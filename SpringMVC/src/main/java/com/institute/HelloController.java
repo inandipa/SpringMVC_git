1664829f27431673c2670fd73ee82cdde77bad23
inandipa <inandipa@uncc.edu>
2016-03-30 18:12:24 -0400

    first commit

---
+++ SpringMVC/src/main/java/com/institute/HelloController.java
@@ -0,0 +1,31 @@
+package com.institute;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+
+ public class HelloController extends AbstractController {
+   
+	protected ModelAndView handleRequestIntrenal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception{
+
+		 ModelAndView modelandview = new ModelAndView("HelloPage");
+		 modelandview.addObject("welcomemessage","iwghfskgkjsbrgkjbrsk");
+		 
+		 return modelandview;
+	}
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+	
+}
---
+++ SpringMVC/src/main/webapp/WEB-INF/HelloPage.jsp
@@ -0,0 +1,11 @@
+<html>
+<head>
+
+<title>Insert title here</title>
+</head>
+<body>
+
+<h1>${welcomemessage }</h1>
+
+</body>
+</html>
